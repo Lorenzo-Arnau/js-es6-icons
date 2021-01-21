02fc71c541fa6147174dd1120cc830a5ad058269
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-21 16:42:29 +0100

    update,add append and switch case

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// Milestone 1:
-// - Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
-// - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
+// Milestone 1: - Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia. - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
 const prefisso = 'fas fa-';
 const tipo = 'icon';
 const icons =[
@@ -101,22 +99,21 @@ const icons =[
     family: 'user',
   },
 ];
-console.log(icons);
-// Milestone 2:
-// - Definire un array di colori e associare ad ogni tipo di icona un colore.
-// - Visualizzare le icone di colore diverso in base al tipo.
+// Milestone 2: - Definire un array di colori e associare ad ogni tipo di icona un colore. - Visualizzare le icone di colore diverso in base al tipo.
 const colors = ['orange','blue','purple']
 const container = document.getElementsByClassName('containerIcons')[0];
 let familyType;
 icons.forEach((item) => {
-  if (item.family == 'animals') {
-    familyType = colors[1]
-  }
-  if (item.family == 'vegetable') {
-    familyType = colors[0]
-  }
-  if (item.family == 'user') {
-    familyType = colors[2]
+  switch (item.family) {
+    case 'animals':
+      familyType = colors[1]
+      break;
+    case 'vegetable':
+      familyType = colors[0]
+      break;
+    case 'user':
+      familyType = colors[2]
+      break;
   }
 container.innerHTML += `
 <div class="box">
@@ -125,12 +122,9 @@ ${item.name}
 </div>
 `
 });
-// Milestone 3:
-// - Aggiungere una select per filtrare le icone in base al tipo.
-// - Popolare le options della select dinamicamente e, ogni volta che cambia il valore selezionato, visualizzare le icone corrispondenti.
+// Milestone 3: - Aggiungere una select per filtrare le icone in base al tipo. - Popolare le options della select dinamicamente e, ogni volta che cambia il valore selezionato, visualizzare le icone corrispondenti.
 
 const selector = $('.selector');
-const familySelector = document.getElementsByClassName('selector')[0];
 const familyList =[];
 
 icons.forEach((item) => {
@@ -140,42 +134,47 @@ icons.forEach((item) => {
 });
 console.log(familyList);
 familyList.forEach((element) => {
-  familySelector.innerHTML += `
+  selector.append(`
   <option value="${element}">${element}</option>
-  `
+  `)
 });
 
 selector.change(function(){
   let valore = $(this).val();
   let visualize;
-  if (valore == 'animals') {
+  switch (valore) {
+    case 'animals':
     visualize = icons.filter((element)=>{
       return element.family == 'animals'
     })
-  }
-  if (valore == 'vegetable') {
+    break;
+    case 'vegetable':
     visualize = icons.filter((element)=>{
       return element.family == 'vegetable'
     })
-  }
-  if (valore == 'user') {
+    break;
+    case 'user':
     visualize = icons.filter((element)=>{
       return element.family == 'user'
     })
-  }
-  if (valore == 'all') {
+    break;
+    default:
     visualize = icons
-  }
-  container.innerHTML = ''
+    }
+
+  container.innerHTML = '';
+
   visualize.forEach((item) => {
-    if (valore == 'animals' || item.family == 'animals') {
-      familyType = colors[1]
-    }
-    if (valore == 'vegetable'|| item.family == 'vegetable') {
-      familyType = colors[0]
-    }
-    if (valore == 'user'|| item.family == 'user') {
-      familyType = colors[2]
+    switch (item.family) {
+      case 'animals':
+        familyType = colors[1]
+      break;
+      case 'vegetable':
+        familyType = colors[0]
+      break;
+      case 'user':
+        familyType = colors[2]
+      break;
     }
     container.innerHTML += `
     <div class="box">
@@ -184,5 +183,4 @@ selector.change(function(){
     </div>
     `
   });
-  console.log(visualize);
 })
