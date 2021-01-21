b92a6034bdd92cfe79a9c714b6f136cc81cabe40
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-21 15:32:55 +0100

    milestone 3 done

--- js/script.js
+++ js/script.js
@@ -106,8 +106,6 @@ console.log(icons);
 // - Definire un array di colori e associare ad ogni tipo di icona un colore.
 // - Visualizzare le icone di colore diverso in base al tipo.
 const colors = ['orange','blue','purple']
-
-
 const container = document.getElementsByClassName('containerIcons')[0];
 let familyType;
 icons.forEach((item) => {
@@ -127,3 +125,61 @@ ${item.name}
 </div>
 `
 });
+// Milestone 3:
+// - Aggiungere una select per filtrare le icone in base al tipo.
+// - Popolare le options della select dinamicamente e, ogni volta che cambia il valore selezionato, visualizzare le icone corrispondenti.
+
+const selector = $('.selector');
+const familySelector = document.getElementsByClassName('selector')[0];
+const familyList =[];
+
+icons.forEach((item) => {
+  if(!familyList.includes(item.family)){
+    familyList.push(item.family)
+  }
+});
+console.log(familyList);
+familyList.forEach((element) => {
+  familySelector.innerHTML += `
+  <option value="${element}">${element}</option>
+  `
+});
+
+selector.change(function(){
+  let valore = $(this).val();
+  let visualize;
+  if (valore == 'animals') {
+    visualize = icons.filter((element)=>{
+      return element.family == 'animals'
+    })
+  }
+  if (valore == 'vegetable') {
+    visualize = icons.filter((element)=>{
+      return element.family == 'vegetable'
+    })
+  }
+  if (valore == 'user') {
+    visualize = icons.filter((element)=>{
+      return element.family == 'user'
+    })
+  }
+  container.innerHTML = ''
+  visualize.forEach((item) => {
+    if (valore == 'animals') {
+      familyType = colors[1]
+    }
+    if (valore == 'vegetable') {
+      familyType = colors[0]
+    }
+    if (valore == 'user') {
+      familyType = colors[2]
+    }
+    container.innerHTML += `
+    <div class="box">
+    <i class="${item.prefisso}${item.name}" style="color:${familyType}"></i> </br>
+    ${item.name}
+    </div>
+    `
+  });
+  console.log(visualize);
+})
